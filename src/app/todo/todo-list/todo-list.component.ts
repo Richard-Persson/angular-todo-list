e75356ad32bfac0aec6633ef82c54c8f62d97622
Richard Persson <ricpersson00@gmail.com>
2025-09-11 09:13:32 +0200

    completed excercise with the help of copilot :)

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
 
@@ -7,17 +7,27 @@ import { Todo } from '../models/todo';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
   constructor(private readonly todoService: TodoService) {}
+  todos: Todo[] = [];
+  showCompleted: boolean = false;
 
-  todos = this.todoService.getTodos()
-
+  async ngOnInit(): Promise<void>{
+    this.todos = await this.todoService.getTodos();
+  }
+  
+  get filteredTodos(): Todo[] {
+    return this.todos.filter(todo => todo.completed === this.showCompleted);
+  }
+toggleShowCompleted() {
+    this.showCompleted = !this.showCompleted;
+}
   updateTodo(todo: Todo) {
     this.todoService.updateTodo(todo);
   }
 
   async newTodo(title: string) {
     await this.todoService.addTodo(title);
-    this.todos = this.todoService.getTodos();
+    this.todos = await this.todoService.getTodos();
   }
 }
